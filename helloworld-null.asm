172bb21a02b0cea40ac8547f8fc35480831b3c80
DeLuks <87036369+DeLuks2006@users.noreply.github.com>
2023-10-13 22:18:11 +0200

    Add files via upload

---
+++ helloworld-null.asm
@@ -0,0 +1,22 @@
+; hello world program (NULL terminating bytes)
+; helloworld-inc.asm
+
+%include	'functions.asm'		; include our external file
+
+SECTION .data
+msg1	db	'Hello, brave new world!', 0Ah, 0h
+msg2	db	'This is how we recycle in NASM.', 0AH, 0h
+
+SECTION .text
+global	_start
+
+_start:
+	mov	eax, msg1
+	call	sprint
+
+	mov	eax, msg2
+	call	sprint
+
+	call 	quit
+
+; ERROR - message is outputted twice
